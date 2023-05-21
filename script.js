c18410f76086a2b9ee0af10a8ffcd1ee6ec4f2ff
krystyna-lyn <krystyna_kh@gmx.de>
2023-05-22 00:52:47 +0200

    update

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 let menu = document.querySelector('#menu-btn');
 let navbar = document.querySelector('.header .navbar');
 
-menu.onclick =() =>{
+menu.onclick = () => {
     menu.classList.toggle('fa-times');
     navbar.classList.toggle('active');
+}
+
+window.onsccroll = () => {
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
 }
